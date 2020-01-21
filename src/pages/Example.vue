14225cb8095b018b89ed5ec07088a4b5b8499009
Charlie Lee <charlie800228@gmail.com>
2020-01-21 11:03:15 +0800

    Add require-component-is issue

--- src/pages/Example.vue
+++ src/pages/Example.vue
@@ -2,6 +2,10 @@
   <div>
     <h1>Example Page Component</h1>
     {{ pageData }}
+    <!-- eslint-disable-next-line vue/require-component-is -->
+    <component :is="componentType">
+      {{ componentType }}
+    </component>
   </div>
 </template>
 
@@ -11,14 +15,15 @@ import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
 
 export default {
   methods: {
-    test() {
-
-    }
+    test() {},
   },
   computed: {
     pageData() {
       return Date.now()
-    }
-  }
+    },
+    componentType() {
+      return this.pageData % 2 ? 'button' : 'p'
+    },
+  },
 } as ThisTypedComponentOptionsWithRecordProps<Vue, {}, {}, {}, {}>
 </script>
